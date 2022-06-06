e3b1a7b4da5db045b6b71289b3ebfea393d98601
32th-System <intensonat@gmail.com>
2022-06-06 18:25:00 +0200

    Update version number

--- thprac/src/thprac/thprac_version.cpp
+++ thprac/src/thprac/thprac_version.cpp
@@ -1,8 +1,8 @@
 #include "thprac_version.h"
 
 namespace THPrac {
-const char* g_thpracVersionStr { "2.1" };
-const int g_thpracVersionInt[] { 2, 1, 0, 0 };
+const char* g_thpracVersionStr { "2.1.1" };
+const int g_thpracVersionInt[] { 2, 1, 1, 0 };
 
 
 const char* GetVersionStr()
