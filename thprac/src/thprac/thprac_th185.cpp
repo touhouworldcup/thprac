51b805bf7c821d25e583b0b1142f96a446ef6247
muter3000 <muter3000@gmail.com>
2022-09-05 00:45:31 +0200

    th185: kapppa

--- thprac/src/thprac/thprac_th185.cpp
+++ thprac/src/thprac/thprac_th185.cpp
@@ -12,7 +12,7 @@ namespace TH185 {
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18, 0x00, 0x00, 0x00, 0xFF, 0x02, 0x00, 0x00, 0x00, 0x00, 0x30, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, \
         0x00, 0x00, 0x00, 0x00, 0xF3 * (boss_index == 3), 0x03 * (boss_index == 3), 0x18, 0x00, 0x00, 0x00, 0xFF, 0x02, 0x00, 0x00, 0x00, 0x00, (start_index) + 3, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00
     
-    #define BOSS_SPELL_CARD_ASYNC(boss_id) 0x00, 0x00, 0x00, 0x00, 0x0B, 0x00, 0x24, 0x00, 0x00, 0x00, 0xFF, 0x01, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x42, 0x6F, 0x73, 0x73, boss_id / 10 + 0x30, boss_id % 10 + 0x30, 0x42, 0x6F, 0x73, 0x73, 0x43, 0x61, 0x72, 0x64, 0x31, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0A, 0x00, 0x10, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00
+    #define BOSS_SPELL_CARD_ASYNC(boss_id, spell_id) 0x00, 0x00, 0x00, 0x00, 0x0B, 0x00, 0x24, 0x00, 0x00, 0x00, 0xFF, 0x01, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x42, 0x6F, 0x73, 0x73, boss_id / 10 + 0x30, boss_id % 10 + 0x30, 0x42, 0x6F, 0x73, 0x73, 0x43, 0x61, 0x72, 0x64, 0x30+spell_id, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0A, 0x00, 0x10, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00
     enum addrs {
         BLACK_MARKET_PTR = 0x4d7ac4,
         CARD_DESC_LIST = 0x4ca370
@@ -495,7 +495,7 @@ namespace TH185 {
                                     { "Boss01Boss1", {
                                         // @Boss01BossCard1();
                                         // ret();
-                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(1) } }
+                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(1, 1) } }
                                     } }
                                 }
                             }
@@ -517,7 +517,7 @@ namespace TH185 {
                                     { "Boss02Boss1", {
                                         // @Boss02BossCard1();
                                         // ret();
-                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(2) } }
+                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(2, 1) } }
                                     } }
                                 }
                             }
@@ -539,7 +539,7 @@ namespace TH185 {
                                     { "Boss03Boss1", {
                                         // @Boss03BossCard1();
                                         // ret();
-                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(3) } }
+                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(3, 1) } }
                                     } }
                                 }
                             }
@@ -561,7 +561,7 @@ namespace TH185 {
                                     { "Boss04Boss1", {
                                         // @Boss04BossCard1();
                                         // ret();
-                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(4) } }
+                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(4, 1) } }
                                     } }
                                 }
                             }
@@ -678,7 +678,7 @@ namespace TH185 {
                                     { "Boss05Boss1", {
                                         // @Boss05BossCard1();
                                         // ret();
-                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(5) } }
+                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(5, 1) } }
                                     } }
                                 }
                             }
@@ -700,7 +700,7 @@ namespace TH185 {
                                     { "Boss06Boss1", {
                                         // @Boss06BossCard1();
                                         // ret();
-                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(6) } }
+                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(6, 1) } }
                                     } }
                                 }
                             }
@@ -722,7 +722,7 @@ namespace TH185 {
                                     { "Boss07Boss1", {
                                         // @Boss07BossCard1();
                                         // ret();
-                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(7)} }
+                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(7, 1)} }
                                     } }
                                 }
                             }
@@ -744,7 +744,7 @@ namespace TH185 {
                                     { "Boss08Boss1", {
                                         // @Boss08BossCard1();
                                         // ret();
-                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(8) } }
+                                        { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(8, 1) } }
                                     } }
                                 }
                             }
@@ -876,7 +876,7 @@ namespace TH185 {
                                     { "Boss09Boss1", {
                                         // @Boss09BossCard1();
                                         // ret();
-                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(9) } }
+                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(9, 1) } }
                                     } }
                                 }
                             }
@@ -899,7 +899,7 @@ namespace TH185 {
                                     { "Boss10Boss1", {
                                         // @Boss10BossCard1();
                                         // ret();
-                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(10) } }
+                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(10, 1) } }
                                     } }
                                 }
                             }
@@ -922,7 +922,7 @@ namespace TH185 {
                                     { "Boss11Boss1", {
                                         // @Boss12BossCard1();
                                         // ret();
-                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(11) } }
+                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(11, 1) } }
                                     } }
                                 }
                             }
@@ -945,7 +945,7 @@ namespace TH185 {
                                     { "Boss12Boss1", {
                                         // @Boss12BossCard1();
                                         // ret();
-                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(12) } }
+                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(12, 1) } }
                                     } }
                                 }
                             }
@@ -1057,6 +1057,20 @@ namespace TH185 {
                                         { .off = 0x34, .bytes = { FORCE_BOSS(3, 14) } }
                                     } }
                                 }
+                            }, 
+                            {
+                                .label = "Nitori Kawashiro",
+                                .writes = {
+                                    { "WorldWaveB00", {
+                                        { .off = 0x34, .bytes = {  
+                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18, 0x00, 0x00, 0x00, 0xFF, 0x02, 0x00, 0x00, 0x00, 0x00, 14, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18, 0x00, 0x00, 0x00, 0xFF, 0x02, 0x00, 0x00, 0x00, 0x00, 15, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18, 0x00, 0x00, 0x00, 0xFF, 0x02, 0x00, 0x00, 0x00, 0x00, 16, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x14, 0x00, 0x01, 0x00, 0xFF, 0x01, 0x00, 0x00, 0x00, 0x00, 0x5E, 0xD9, 0xFF, 0xFF,
+                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18, 0x00, 0x00, 0x00, 0xFF, 0x02, 0x00, 0x00, 0x00, 0x00, 0x30, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                                        0x00, 0x00, 0x00, 0x00, 0xF3, 0x03, 0x18, 0x00, 0x00, 0x00, 0xFF, 0x02, 0x00, 0x00, 0x00, 0x00, 27, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00 } }
+                                    } }
+                                }
                             }
                         }
                     };
@@ -1077,7 +1091,7 @@ namespace TH185 {
                                     { "Boss13Boss1", {
                                         // @Boss13BossCard1();
                                         // ret();
-                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(13) } }
+                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(13, 1) } }
                                     } }
                                 }
                             }
@@ -1100,7 +1114,7 @@ namespace TH185 {
                                     { "Boss14Boss1", {
                                         // @Boss14BossCard1();
                                         // ret();
-                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(14) } }
+                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(14, 1) } }
                                     } }
                                 }
                             }
@@ -1123,7 +1137,7 @@ namespace TH185 {
                                     { "Boss15Boss1", {
                                         // @Boss15BossCard1();
                                         // ret();
-                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(15) } }
+                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(15, 1) } }
                                     } }
                                 }
                             }
@@ -1146,7 +1160,53 @@ namespace TH185 {
                                     { "Boss16Boss1", {
                                         // @Boss16BossCard1();
                                         // ret();
-                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(16) } }
+                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(16, 1) } }
+                                    } }
+                                }
+                            }
+                        }
+                    };
+
+                    stage.section_param[5].phases->section_param[5].phases = {
+                        .label = "Attack",
+                        .type = stage_warps_t::TYPE_COMBO,
+                        .section_param = {
+                            {
+                                .label = "Dialogue"
+                            },
+                            {
+                                .label = "Nonspell",
+                                .writes = {
+                                    { "Boss26Boss", {
+                                        { .off = 0x208, .bytes = {  0x00, 0x00, 0x24, 0x00 } }
+                                    } },
+                                }
+                            },
+                            {
+                                .label = "Weapon \"Water Flamethrower\"",
+                                .writes = {
+                                    { "Boss26Boss", {
+                                        { .off = 0x1b0, .bytes = { 0xa0, 0x0f, 0x00, 0x00 } },
+                                        { .off = 0x208, .bytes = { 0x00, 0x00, 0x24, 0x00 } }
+                                    } },
+                                    { "Boss26Boss1", {
+                                        // @Boss26BossCard1();
+                                        // ret();
+                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(26, 1) } }
+                                    } }
+                                }
+                            },
+                            {
+                                .label = "\"Like the Flow of the Mountain Marsh\"",
+                                .writes = {
+                                    { "Boss26Boss", {
+                                        { .off = 0x1b0, .bytes = { 0xd0, 0x07, 0x00, 0x00 } },
+                                        { .off = 0x208, .bytes = { 0x00, 0x00, 0x24, 0x00 } }
+                                    } },
+                                    { "Boss26Boss1", {
+                                        // @Boss26BossCard1();
+                                        // ret();
+                                    { .off = 0x10, .bytes = { BOSS_SPELL_CARD_ASYNC(26, 2) } }
                                     } }
                                 }
                             }
