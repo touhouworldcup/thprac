7537b1e22e62aefffb537ba8c35421abb74589a7
32th-System <intensonat@gmail.com>
2023-09-04 14:17:50 +0200

    TH19: advanced options
    And now it's time for the TH19 version of the TH09 tools

--- thprac/src/thprac/thprac_th19.cpp
+++ thprac/src/thprac/thprac_th19.cpp
@@ -3,14 +3,117 @@
 
 namespace THPrac {
 namespace TH19 {
+    class THAdvOptWnd : public Gui::GameGuiWnd {
+        // Option Related Functions
+    private:
+        void FpsInit()
+        {
+            if (*(uint8_t*)RVA(0x208621) == 3) {
+                mOptCtx.fps_status = 1;
+
+                DWORD oldProtect;
+                VirtualProtect((void*)RVA(0xA95B6), 4, PAGE_EXECUTE_READWRITE, &oldProtect);
+                *(double**)RVA(0xA95B6) = &mOptCtx.fps_dbl;
+                VirtualProtect((void*)RVA(0xA95B6), 4, oldProtect, &oldProtect);
+            } else
+                mOptCtx.fps_status = 0;
+        }
+        void FpsSet()
+        {
+            if (mOptCtx.fps_status == 1) {
+                mOptCtx.fps_dbl = 1.0 / (double)mOptCtx.fps;
+            }
+        }
+        void GameplayInit()
+        {
+        }
+        void GameplaySet()
+        {
+        }
+
+    public:
+        THAdvOptWnd() noexcept
+        {
+            SetWndFlag(ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove);
+            SetFade(0.8f, 0.8f);
+            SetStyle(ImGuiStyleVar_WindowRounding, 0.0f);
+            SetStyle(ImGuiStyleVar_WindowBorderSize, 0.0f);
+            OnLocaleChange();
+
+            FpsInit();
+            GameplayInit();
+        }
+
+    protected:
+        virtual void OnLocaleChange() override
+        {
+            SetTitle(S(TH_ADV_OPT));
+            switch (Gui::LocaleGet()) {
+            case Gui::LOCALE_ZH_CN:
+                SetSizeRel(1.0f, 1.0f);
+                SetPosRel(0.0f, 0.0f);
+                SetItemWidthRel(-0.075f);
+                SetAutoSpacing(true);
+                break;
+            case Gui::LOCALE_EN_US:
+                SetSizeRel(1.0f, 1.0f);
+                SetPosRel(0.0f, 0.0f);
+                SetItemWidthRel(-0.075f);
+                SetAutoSpacing(true);
+                break;
+            case Gui::LOCALE_JA_JP:
+                SetSizeRel(1.0f, 1.0f);
+                SetPosRel(0.0f, 0.0f);
+                SetItemWidthRel(-0.075f);
+                SetAutoSpacing(true);
+                break;
+            default:
+                break;
+            }
+        }
+        virtual void OnContentUpdate() override
+        {
+            ImGui::TextUnformatted(S(TH_ADV_OPT));
+            ImGui::Separator();
+            ImGui::BeginChild("Adv. Options", ImVec2(0.0f, 0.0f));
+
+            if (BeginOptGroup<TH_GAME_SPEED>()) {
+                if (GameFPSOpt(mOptCtx, false))
+                    FpsSet();
+                EndOptGroup();
+            }
+
+            AboutOpt();
+            ImGui::EndChild();
+            ImGui::SetWindowFocus();
+        }
+
+        adv_opt_ctx mOptCtx;
+    };
+    bool UpdateAdvOptWindow()
+    {
+        static THAdvOptWnd* advOptWnd = nullptr;
+        if (!advOptWnd)
+            advOptWnd = new THAdvOptWnd();
+        if (Gui::KeyboardInputUpdate(VK_F12) == 1) {
+            if (advOptWnd->IsOpen())
+                advOptWnd->Close();
+            else
+                advOptWnd->Open();
+        }
+        advOptWnd->Update();
+
+        return advOptWnd->IsOpen();
+    }
+
     HOOKSET_DEFINE(THMainHook)
 
-    EHOOK_DY(th19_update, 0xC8B75) {
+    EHOOK_DY(th19_update_begin, 0xC89E0) {
         GameGuiBegin(IMPL_WIN32_DX9);
+    }
 
-        ImGui::TextUnformatted("Deez Nuts");
-
-        GameGuiEnd();
+    EHOOK_DY(th19_update_end, 0xC8B75) {
+        GameGuiEnd(UpdateAdvOptWindow());
     }
 
     EHOOK_DY(th19_render, 0xC8C8D) {
