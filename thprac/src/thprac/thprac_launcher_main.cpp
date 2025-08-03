d67b2bb8e36b8e7acfe278edeed90110be6ca390
32th-System <intensonat@gmail.com>
2025-08-04 01:02:19 +0200

    Launcher: remove custom themes
    Not sure what I was thinking when I implemented this

--- thprac/src/thprac/thprac_launcher_main.cpp
+++ thprac/src/thprac/thprac_launcher_main.cpp
@@ -68,29 +68,7 @@ int GuiLauncherMain()
 
     int theme;
     if (LauncherSettingGet("theme", theme)) {
-        // LauncherSettingSet doesn't take int, only int&.
-        // Passing 0 will call the overload with const char*
-        int Sus = 0;
-        const char* theme_user = nullptr;
-        // LauncherSettingGet only accepts signed ints but I want to do an unsigned comparison
-        if ((unsigned int)theme > 2) {
-            if (LauncherSettingGet("theme_user", theme_user) && theme_user) {
-                std::wstring theme_path = LauncherGetDataDir() + L"themes\\" + utf8_to_utf16(theme_user);
-                if (!PathFileExistsW(theme_path.c_str())) {
-                    LauncherSettingSet("theme", Sus);
-                    theme = Sus;
-                }
-            } else {
-                LauncherSettingSet("theme", Sus);
-                theme = Sus;
-            }
-        }
-
-        if (theme_user) {
-            SetTheme(theme, utf8_to_utf16(theme_user).c_str());
-        } else {
-            SetTheme(theme);
-        }
+        SetTheme(theme);
     }
     LauncherPeekUpd();
     auto scale = LauncherWndGetScale();
