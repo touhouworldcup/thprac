fbe0ecf7a36c90eb03cdfaf92a4ed0e330b74871
Nylilsa <nylilsa2468@gmail.com>
2025-09-04 00:47:06 +0200

    very important typo fix that someone pointed out in a discord chat

--- thprac/src/thprac/thprac_locale_def.cpp
+++ thprac/src/thprac/thprac_locale_def.cpp
@@ -1837,7 +1837,7 @@ const char* th_glossary_str[3][1048]
         "Fatal error occurred while initializing thprac.",
         "No valid game was found.",
         "Failed to run game.",
-        "Applied thprac.\nIf nothing happend, return to main menu in game once.",
+        "Applied thprac.\nIf nothing happened, return to main menu in game once.",
         "Purge",
         "Reflective launch",
         "Enable if the game behaves abnormally when launched by thprac but not when launched directly.\nThis will nullify any functionality that requires a direct game launch from thprac.",
