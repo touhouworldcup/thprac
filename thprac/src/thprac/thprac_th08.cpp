6185fa999ad93bdfc83cb028152146232d5f0bae
32th-System <intensonat@gmail.com>
2026-03-17 00:03:08 +0100

    th08: move TableSetupColumn calls for tracker table to right after BeginTable
    This is where TableSetupColumn is supposed to go
    
    fixes #337

--- thprac/src/thprac/thprac_th08.cpp
+++ thprac/src/thprac/thprac_th08.cpp
@@ -2272,6 +2272,9 @@ namespace TH08 {
         ImGui::TextUnformatted(buf);
 
         ImGui::BeginTable("Tracker table", 2);
+        ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthStretch, 2);
+        ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthStretch, 1);
+        
         ImGui::TableNextRow();
 
         ImGui::TableNextColumn();
@@ -2280,9 +2283,7 @@ namespace TH08 {
         ImGui::Text("%d (%d)", GetMemContent(MISS_COUNT_ADDR), tracker_info.th08.disslove_count);
 
         ImGui::TableNextRow();
-        ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthStretch, 2);
-        ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthStretch, 1);
-
+        
         ImGui::TableNextColumn();
         ImGui::TextUnformatted(S(TH_TRACKER_BOMB));
         ImGui::TableNextColumn();
