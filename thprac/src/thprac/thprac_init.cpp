b01119896c7d1ff7f12b73afc9e623b1d10b7361
32th-System <intensonat@gmail.com>
2026-04-03 23:37:45 +0200

    Combine main.cpp and thprac_init.cpp into one file

--- thprac/src/thprac/thprac_init.cpp
+++ thprac/src/thprac/thprac_init.cpp
@@ -1,7 +1,4 @@
-﻿#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-
-#include <Windows.h>
+﻿#define NOMINMAX
 #include "utils/wininternal.h"
 
 #include "thprac_identify.h"
@@ -9,10 +6,10 @@
 #include "thprac_hook.h"
 #include "thprac_cfg.h"
 #include "thprac_log.h"
-
-#pragma intrinsic(strcmp)
+#include "thprac_gui_locale.h"
 
 namespace THPrac {
+extern int Launcher(HINSTANCE hInstance, int nCmdShow);
 
 EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 void RemoteInit() {
@@ -27,6 +24,131 @@ void RemoteInit() {
         ExitThread(0);
     } else {
         FreeLibraryAndExitThread((HMODULE)&__ImageBase, 0);
-    }    
+    }
 }
+
+bool PrivilegeCheck() {
+    bool ret = false;
+
+    HANDLE hToken;
+    if (!NtOpenProcessToken(CurrentProcessHandle, TOKEN_QUERY, &hToken)) {
+        TOKEN_ELEVATION Elevation;
+        ULONG cbSize = sizeof(Elevation);
+        if (!NtQueryInformationToken(hToken, TokenElevation, &Elevation, cbSize, &cbSize)) {
+            ret = Elevation.TokenIsElevated;
+        }
+        CloseHandle(hToken);
+    }
+    return ret;
+}
+}
+
+using namespace THPrac;
+int WINAPI wWinMain(HINSTANCE hInstance, [[maybe_unused]] HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+    InitConfigDir();
+    LoadSettings();
+
+    srand(Kuser_Shared_Data->SystemTime.LowPart);
+    RemoteInit();
+
+    log_init(true, gSettings.console);
+
+    if (gSettings.thprac_admin_rights && !PrivilegeCheck()) {
+        ShellExecuteW(NULL, L"runas", CurrentPeb()->ProcessParameters->ImagePathName.Buffer, nullptr, nullptr, nCmdShow);
+    }
+
+    int argc = 0;
+    wchar_t** argv = CommandLineToArgvW(pCmdLine, &argc);
+    defer(LocalFree(argv));
+
+    enum CurrentCmd {
+        CMD_NONE,
+        CMD_ATTACH
+    };
+
+    CurrentCmd curCmd = CMD_NONE;
+    uint32_t flags = 0xFFFFFFFF;
+
+    for (int i = 0; i < argc; i++) {
+        if (curCmd == CMD_ATTACH) {
+            curCmd = CMD_NONE;
+
+            int pid = _wtoi(argv[i]);
+            if (ApplyToProcById(pid)) {
+                return 0;
+            }
+            else {
+                fprintf(stderr, "Warning: invalid PID: %d\n", pid);
+                continue;
+            }
+        }
+
+        if (wcscmp(argv[i], L"--attach") == 0) {
+            curCmd = CMD_ATTACH;
+            continue;
+        }
+
+        if (wcscmp(argv[i], L"--without-vpatch") == 0) {
+            flags &= ~RUN_FLAG_VPATCH;
+            continue;
+        }
+
+        if (wcscmp(argv[i], L"--without-oilp") == 0) {
+            flags &= ~RUN_FLAG_OILP;
+            continue;
+        }
+
+        if (GetFileAttributesW(argv[i]) != INVALID_FILE_ATTRIBUTES) {
+            auto& self_exe = CurrentPeb()->ProcessParameters->ImagePathName;
+            if (memcmp(argv[i], self_exe.Buffer, self_exe.Length) == 0) {
+                continue;
+            }
+            wchar_t* launch_cmdline = wcsstr(pCmdLine, argv[i]) + t_strlen(argv[i]);
+
+            if (RunGame(argv[i], launch_cmdline, flags)) {
+                return 0;
+            }
+        }
+    }
+
+    if (curCmd == CMD_ATTACH) {
+        FindAndAttach(false, false);
+        return 0;
+    }
+    else if (!gSettings.dont_search_ongoing_game && FindAndAttach(false, true)) {
+        return 0;
+    }
+
+    // I already need all of this to have it's own scope.
+    if (gSettings.existing_game_launch_action != LAUNCH_ACTION_OPEN_LAUNCHER) {
+        WIN32_FIND_DATAW find = {};
+        HANDLE hFind = FindFirstFileW(L"*.exe", &find);
+        if (!hFind) {
+            return false;
+        }
+        do {
+            const auto* ver = IdentifyExe(find.cFileName);
+            if (!ver) {
+                continue;
+            }
+
+            if (gSettings.existing_game_launch_action == LAUNCH_ACTION_ALWAYS_ASK) {
+                int choice = log_mboxf(0, MB_YESNOCANCEL, S(THPRAC_EXISTING_GAME_CONFIRMATION_TITLE), S(THPRAC_EXISTING_GAME_CONFIRMATION), gThGameStrs[ver->gameId]);
+                if (choice == IDYES || choice == IDCANCEL) {
+                    goto run_this_game;
+                }
+                else {
+                    continue;
+                }
+            }
+            else {
+            run_this_game:
+                FindClose(hFind);
+                RunGame(find.cFileName, nullptr, flags);
+                return 0;
+            }
+        } while (FindNextFileW(hFind, &find));
+        FindClose(hFind);
+    }
+    return Launcher(hInstance, nCmdShow);
 }
