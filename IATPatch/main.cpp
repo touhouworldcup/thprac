506267768644fba66a0cb9c78cbfd268c3864114
Invex <invex@foxmail.com>
2026-07-30 19:12:42 +0800

    change compatiable solution

---
+++ IATPatch/main.cpp
@@ -0,0 +1,120 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+DWORD RvaToOffset(DWORD rva, IMAGE_NT_HEADERS32* nt)
+{
+    IMAGE_SECTION_HEADER* section = IMAGE_FIRST_SECTION(nt);
+
+    for (WORD i = 0; i < nt->FileHeader.NumberOfSections; i++, section++) {
+        DWORD va = section->VirtualAddress;
+
+        DWORD size = section->Misc.VirtualSize;
+
+        if (rva >= va && rva < va + size) {
+            return section->PointerToRawData
+                + (rva - va);
+        }
+    }
+
+    return 0;
+}
+
+
+bool PatchAPI(
+    const char* input,
+    const char* output)
+{
+    if (!CopyFileA(input, output, FALSE)) {
+        printf("Copy failed: %lu\n", GetLastError());
+        return false;
+    }
+
+    HANDLE hFile = CreateFileA(output, GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, nullptr);
+
+    if (hFile == INVALID_HANDLE_VALUE)
+        return false;
+
+    HANDLE hMap = CreateFileMappingA(hFile, nullptr, PAGE_READWRITE, 0, 0, nullptr);
+
+    if (!hMap) {
+        CloseHandle(hFile);
+        return false;
+    }
+
+    BYTE* base = (BYTE*)MapViewOfFile(hMap, FILE_MAP_WRITE, 0, 0, 0);
+
+    if (!base) {
+        CloseHandle(hMap);
+        CloseHandle(hFile);
+        return false;
+    }
+
+    bool patched = false;
+
+    IMAGE_DOS_HEADER* dos = (IMAGE_DOS_HEADER*)base;
+
+    IMAGE_NT_HEADERS32* nt = (IMAGE_NT_HEADERS32*)(base + dos->e_lfanew);
+
+    auto& dir = nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+
+    IMAGE_IMPORT_DESCRIPTOR* imp = NULL; 
+     DWORD importOffset = 0;
+    if (dir.VirtualAddress == 0)
+        goto finish;
+
+    
+    importOffset = RvaToOffset(dir.VirtualAddress, nt);
+    imp = (IMAGE_IMPORT_DESCRIPTOR*)(base + importOffset);
+
+    for (; imp->Name; imp++) {
+        char* dll = (char*)(base + RvaToOffset(imp->Name, nt));
+
+        if (_stricmp(dll, "KERNEL32.dll") != 0)
+            continue;
+        DWORD thunkOffset = RvaToOffset(imp->OriginalFirstThunk, nt);
+
+        if (!imp->OriginalFirstThunk) {
+            thunkOffset = RvaToOffset(imp->FirstThunk, nt);
+        }
+
+        IMAGE_THUNK_DATA* thunk = (IMAGE_THUNK_DATA*)(base + thunkOffset);
+
+        for (; thunk->u1.AddressOfData; thunk++) {
+            IMAGE_IMPORT_BY_NAME* ibn = (IMAGE_IMPORT_BY_NAME*)(base + RvaToOffset(thunk->u1.AddressOfData, nt));
+            char* name = (char*)ibn->Name;
+            if (strcmp(name, "GetSystemTimePreciseAsFileTime") == 0) {
+                printf("Patch: %s -> GetSystemTimeAsFileTime\n", name);
+                strcpy_s(name, 64, "GetSystemTimeAsFileTime");
+                patched = true;
+            }
+        }
+    }
+
+finish:
+
+    UnmapViewOfFile(base);
+    CloseHandle(hMap);
+    CloseHandle(hFile);
+
+    if (patched)
+        printf("Patch success\n");
+    else
+        printf("No patch needed\n");
+
+    return true;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3) {
+        printf( "Usage:\n IATPatch.exe input.exe output.exe\n");
+        return 1;
+    }
+
+    if (!PatchAPI(argv[1], argv[2])) {
+        printf("Patch failed\n");
+        return 2;
+    }
+
+    return 0;
+}
--- thprac/src/thprac/thprac_compatible.cpp
+++ thprac/src/thprac/thprac_compatible.cpp
@@ -1,22 +1,37 @@
 #include "thprac_compatible.h"
 #include <windows.h>
+#include <time.h>
 
-#if defined(MS_TOOLSET_VERSION)
-#if MS_TOOLSET_VERSION >= 145 // v145
-#define NEED_XTIME_PATCH 1
-#endif
-#endif
+// it's a stupid patch...
+// now just change import table
 
-#if NEED_XTIME_PATCH
-extern "C" {
-__int64 __cdecl _Xtime_get_ticks()
-{
-    FILETIME ft;
-    GetSystemTimeAsFileTime(&ft);
-    ULARGE_INTEGER ui;
-    ui.LowPart = ft.dwLowDateTime;
-    ui.HighPart = ft.dwHighDateTime;
-    return static_cast<__int64>(ui.QuadPart - 116444736000000000ULL);
-}
-}
-#endif
+
+// #if defined(MS_TOOLSET_VERSION)
+// #if MS_TOOLSET_VERSION >= 145 // v145
+// #define NEED_XTIME_PATCH 1
+// #endif
+// #endif
+// 
+// #if NEED_XTIME_PATCH
+// extern "C" {
+// __int64 __cdecl _Xtime_get_ticks()
+// {
+//     FILETIME ft;
+//     GetSystemTimeAsFileTime(&ft);
+//     ULARGE_INTEGER ui;
+//     ui.LowPart = ft.dwLowDateTime;
+//     ui.HighPart = ft.dwHighDateTime;
+//     return static_cast<__int64>(ui.QuadPart - 116444736000000000ULL);
+// }
+// void __cdecl _Timespec64_get_sys(_timespec64 * xt)
+// {
+//     FILETIME ft;
+//     GetSystemTimeAsFileTime(&ft);
+//     unsigned long long t = ((unsigned long long)ft.dwHighDateTime << 32) | ft.dwLowDateTime;
+//     t -= 116444736000000000ULL;
+//     xt->tv_sec = t / 10000000ULL;
+//     xt->tv_nsec = (t % 10000000ULL) * 100;
+// }
+// 
+// }
+// #endif
