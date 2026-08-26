5070369a1ca2c33c02c52ba9e81633c389973eb0
32th-System <intensonat@gmail.com>
2026-08-26 14:57:22 +0200

    th18: move th18_bomb_dec one byte back
    Turns out, overwriting that instruction in particular with NOPs will
    overwrite the `int3` instruction that triggers the hook that counts the
    bomb in the tracker. This caused the tracker to not count bombs if
    infinite bombs were enabled

--- thprac/src/thprac/thprac_th18.cpp
+++ thprac/src/thprac/thprac_th18.cpp
@@ -3934,7 +3934,7 @@ namespace TH18 {
 
     HOOKSET_DEFINE(THMainHook)
     { .addr = 0x44278F, .name = "th18_game_start", .callback = tracker_reset, .data = PatchHookImpl(5) },
-    { .addr = 0x4574D3, .name = "th18_bomb_dec",   .callback = th10_tracker_count_bomb, .data = PatchHookImpl(4) },
+    { .addr = 0x4574D2, .name = "th18_bomb_dec",   .callback = th10_tracker_count_bomb, .data = PatchHookImpl(1) },
     { .addr = 0x410F22, .name = "th18_cylinder",   .callback = th10_tracker_count_bomb, .data = PatchHookImpl(5) },
     { .addr = 0x45D1A3, .name = "th18_life_dec",   .callback = th10_tracker_count_miss, .data = PatchHookImpl(5) },
     { .addr = 0x40DA1C, .name = "th18_notmiss_1",  .callback = th13_tracker_count_trance, .data = PatchHookImpl(5) },
